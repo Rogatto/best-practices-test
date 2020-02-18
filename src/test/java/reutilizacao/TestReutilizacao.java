dfce1bb452578ddc633639e25ec86aadc40d06ed
Guilherme Machado Rogatto <gui.rogatto@gmail.com>
2020-02-17 22:22:46 -0300

    exemplo de reutilização

---
+++ src/test/java/reutilizacao/TestReutilizacao.java
@@ -0,0 +1,52 @@
+package reutilizacao;
+
+import Google.GoogleMenuActions;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class TestReutilizacao {
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp() throws MalformedURLException {
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(CapabilityType.BROWSER_NAME, BrowserType.CHROME);
+        capabilities.setCapability(CapabilityType.PLATFORM_NAME, Platform.LINUX);
+
+        URL remoteWebDriverUrl = new URL("http://localhost:4444/wd/hub");
+        driver = new RemoteWebDriver(remoteWebDriverUrl, capabilities);
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void testZalenium(){
+        driver.get("https://www.google.com.br");
+
+        GoogleMenuActions googleActions = new GoogleMenuActions(driver);
+        googleActions.searchResult("Code Like a Tester");
+
+        Cookie cookie = new Cookie("zaleniumTestPassed", "true");
+        driver.manage().addCookie(cookie);
+    }
+
+    @After
+    public void tearOff(){
+        driver.quit();
+    }
+}
