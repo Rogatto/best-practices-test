62e9805c0df1225dc8f7a40875babda56e559ee9
Guilherme Machado Rogatto <gui.rogatto@gmail.com>
2020-02-26 11:53:35 -0300

    alteraçao

--- src/test/java/contextos/TestFront.java
+++ src/test/java/contextos/TestFront.java
@@ -8,12 +8,12 @@ public class TestFront {
        @Test
        @Category(FrontTest.class)
        public void testFrontFirst() {
-          System.out.println("TEST FRONT 1");
+          System.out.println("TEST FRONT END 1");
        }
 
         @Test
         @Category(FrontTest.class)
         public void testFrontSecond(){
-           System.out.println("TEST FRONT 2");
+           System.out.println("TEST FRONT END 2");
         }
 }
