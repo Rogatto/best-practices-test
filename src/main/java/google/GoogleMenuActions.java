511f6ab4411155096be862d1886dc21a50c79d5d
Guilherme Machado Rogatto <gui.rogatto@gmail.com>
2020-08-25 22:21:59 -0300

    Apresentação TDC

--- src/main/java/google/GoogleMenuActions.java
+++ src/main/java/google/GoogleMenuActions.java
@@ -2,9 +2,6 @@ package google;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class GoogleMenuActions {
 
@@ -18,10 +15,6 @@ public class GoogleMenuActions {
 
     public void searchResult(String text) {
         driver.findElement(searchField).sendKeys(text);
-
-        WebElement myDynamicElement = (new WebDriverWait(driver, 10))
-                .until(ExpectedConditions.elementToBeClickable(buttonSearch));
-
         driver.findElement(buttonSearch).click();
     }
 }
